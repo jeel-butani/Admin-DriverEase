9ba6341273e340ddfce09806c6bce0da950f372c
Hiteshj1693 <Hiteshjethava161611@gmail.com>
2024-04-19 08:13:19 +0530

    springboot

---
+++ Admin-DriveEase-backend/springboot/admindriveease/src/main/java/com/example/admindriveease/AdmindriveeaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.admindriveease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmindriveeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmindriveeaseApplication.class, args);
+	}
+
+}
---
+++ Admin-DriveEase-backend/springboot/admindriveease/src/test/java/com/example/admindriveease/AdmindriveeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admindriveease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmindriveeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
